d372d94eb4275285eaaeab59275ab7bddb1899fd
Lorenzo Kaynan Xavier <lorenzokaynanxavier@gmail.com>
2024-10-17 08:49:41 -0300

    having that good test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Having that good test
         </a>
       </header>
     </div>
